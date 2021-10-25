4d82359c60e8a1b3d95ef3f77cbf84f6c8fc1ce1
Cristóbal Silva Manzo <cristobalsilva@MacBook-Air-de-Cristobal.local>
2021-10-25 09:14:58 -0300

    Add principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'principal',
+    loadChildren: () => import('./pages/principal/principal.module').then( m => m.PrincipalPageModule)
+  },
 ];
 
 @NgModule({
